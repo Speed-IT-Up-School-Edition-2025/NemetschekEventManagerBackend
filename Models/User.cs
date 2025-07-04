d1329bc0e703651cd9a46b6c41fccbbbc20c9c0c
m10ev <mtenev@outlook.com>
2025-07-04 16:16:26 +0300

    Changed DbContext to IdentityDbContext and made user an IdentityUser

--- Models/User.cs
+++ Models/User.cs
@@ -1,20 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
 
 namespace NemetschekEventManagerBackend.Models
 {
-    public class User
+    public class User : IdentityUser
     {
-        [Key]
-        public int Id { get; set; }
-        [Required]
-        public string? Username { get; set; }
-        [Required, EmailAddress]
-        public string? Email { get; set; }
-        [Required, MinLength(6)]
-        public string? PasswordHash { get; set; }
         public DateTime? CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
-        
+
         public ICollection<Submit>? Submissions { get; set; }
     }
 }
