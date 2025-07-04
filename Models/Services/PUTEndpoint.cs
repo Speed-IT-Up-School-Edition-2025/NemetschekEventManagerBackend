ef4bf7379a4109662a1e6cb4626fc67745e5c7c0
lyubomirH <126067409+lyubomirH@users.noreply.github.com>
2025-07-04 17:02:45 +0300

    file for PUT endpoind and folder for all Services

---
+++ Models/Services/PUTEndpoint.cs
@@ -0,0 +1,23 @@
+﻿namespace NemetschekEventManagerBackend.Models.Services
+{
+    public class PUTEndpoint
+    {
+        public static void MapTodoEndpoints(this WebApplication app)
+        {
+            // PUT endpoint
+            app.MapPut("/todoitems/{id}", async (int id, Sub todoToUpdate, TodoDb db) =>
+            {
+                var todo = await db.Todos.FindAsync(id);
+
+                if (todo is null)
+                    return Results.NotFound();
+
+                todo.Name = todoToUpdate.Name;
+                todo.IsComplete = todoToUpdate.IsComplete;
+
+                await db.SaveChangesAsync();
+
+                return Results.NoContent();
+            });
+        }
+}
