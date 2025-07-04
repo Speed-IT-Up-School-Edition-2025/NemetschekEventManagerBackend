6346ef19b512cc6ba327d4555751f4765079ebfd
m10ev <mtenev@outlook.com>
2025-07-04 16:54:09 +0300

    Added services

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using NemetschekEventManagerBackend;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
+// Add services to the container.
 builder.Services.AddDbContext<EventDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); var app = builder.Build();
+// Configure the HTTP request pipeline.
+builder.Services.AddAuthorization();
+// Add Identity services
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<EventDbContext>();
+
+app.MapIdentityApi<IdentityUser>();
 
 app.MapGet("/", () => "Hello World!");
 
