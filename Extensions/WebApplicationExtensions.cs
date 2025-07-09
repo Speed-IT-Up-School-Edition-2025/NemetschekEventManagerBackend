6eb851d90438afeeb9a614e5173f55effc3b62ed
lyubomirH <126067409+lyubomirH@users.noreply.github.com>
2025-07-09 15:05:20 +0300

    implement delete by eventId and userId

--- Extensions/WebApplicationExtensions.cs
+++ Extensions/WebApplicationExtensions.cs
@@ -331,6 +331,19 @@ namespace NemetschekEventManagerBackend.Extensions
 				}
 			}).WithSummary("Removes admin.")
             .WithDescription("Only admins remove other admins which are selected by ID as once the admin role is removed the user gets the role 'User'.");
-		}
+
+            app.MapDelete("/submissions/{eventId}/{userId}",
+            [Authorize(Roles = "Administrator")]
+            (int eventId, string userId, ISubmitService service, ClaimsPrincipal user) =>
+            {
+                var success = service.RemoveUserFromEvent(eventId, userId);
+                return success ? Results.Ok() : Results.NotFound();
+            })
+            .WithSummary("Remove user submission from event by admin")
+            .WithDescription("Allows an admin to remove a user's submission from a specific event by providing the event ID and user ID.");
+
+
+
+        }
 	}
 }
